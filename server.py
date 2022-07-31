3a8b1a807e200559d22bb7566989e1f4302c3f1a
고동우 <shp05179@gmail.com>
2022-07-31 22:22:01 +0900

    add datacollector server

---
+++ server.py
@@ -0,0 +1,18 @@
+from youtubeAPI import youtubeAPI
+import Database as db
+import time
+
+yt = youtubeAPI('AIzaSyBug-zl91U0prwpaI2LgBIg_UHQrv5DP8A')
+
+while True:
+    res = db.search_request_state()
+    if res==-1:
+        print("message received 0")
+    else:
+        recognize, url = res
+        print("message received 1", url)
+        db.update_state_ing(recognize)
+        video_id = url.split('watch?v=')[-1]
+        yt.get_comment_and_likes(recognize, video_id)
+
+    time.sleep(1)
