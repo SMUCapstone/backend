d61a5ecee93a770f54afb630c9499d73a6f664ab
dongwoo <shp05179@gmail.com>
2022-09-08 23:30:05 +0900

    contents api에서 fromDB 삭제

--- app.py
+++ app.py
@@ -89,22 +89,22 @@ def contents():
         if not channelId:
             return ''
 
-        if fromDB:
-            data = db.getContent(channelId)
-            result = []
-            for i in range(len(data.id)):
-                result.append({'id':data.id[i], 'url':data.url[i], 'recognize':data.recognize[i], 'video_name':data.video_name[i], 'thumbnail':data.thumbnail[i], 'hits':str(data.hits[i]), 'comment_num':str(data.comment_num[i]), 'state':str(data.state[i]), 'pageToken':data.last_page[i] })
-            return {'data': result}
+        # if fromDB:
+        #     data = db.getContent(channelId)
+        #     result = []
+        #     for i in range(len(data.id)):
+        #         result.append({'id':data.id[i], 'url':data.url[i], 'recognize':data.recognize[i], 'video_name':data.video_name[i], 'thumbnail':data.thumbnail[i], 'hits':str(data.hits[i]), 'comment_num':str(data.comment_num[i]), 'state':str(data.state[i]), 'pageToken':data.last_page[i] })
+        #     return {'data': result}
 
-        payload = {'channelId':channelId, 'pageToken':pageToken}
-        is_cached = db.search_db_cache(json.dumps(payload))
-        if is_cached:
-            return json.loads(is_cached)
+        # payload = {'channelId':channelId, 'pageToken':pageToken}
+        # is_cached = db.search_db_cache(json.dumps(payload))
+        # if is_cached:
+        #     return json.loads(is_cached)
         if pageToken:
             result = yt.get_contents(channelId, pageToken)
         else:
             result = yt.get_contents(channelId)
-        db.insert_db_cache(json.dumps(payload), json.dumps(result))
+        # db.insert_db_cache(json.dumps(payload), json.dumps(result))
         return result
 
 
